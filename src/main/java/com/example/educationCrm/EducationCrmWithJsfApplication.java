d85655c6feb7c74ac7a863358971beb39e4de231
burakduman-bilgeadam <burkdumn@gmail.com>
2022-08-20 10:50:28 +0300

    change config

--- src/main/java/com/example/educationCrm/EducationCrmWithJsfApplication.java
+++ src/main/java/com/example/educationCrm/EducationCrmWithJsfApplication.java
@@ -4,10 +4,8 @@ package com.example.educationCrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
-@EnableWebMvc
 @SpringBootApplication
 public class EducationCrmWithJsfApplication {
 
