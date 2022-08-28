996283caf2a26a97c299b8c145339d2dd21bdc37
burakduman-bilgeadam <burkdumn@gmail.com>
2022-08-28 11:54:32 +0300

    manyMenu add

--- src/main/java/com/example/educationCrm/controller/TeacherController.java
+++ src/main/java/com/example/educationCrm/controller/TeacherController.java
@@ -3,9 +3,11 @@ package com.example.educationCrm.controller;
 
 import com.example.educationCrm.model.entity.Lesson;
 import com.example.educationCrm.model.entity.School;
+import com.example.educationCrm.model.entity.Student;
 import com.example.educationCrm.model.entity.Teacher;
 import com.example.educationCrm.service.LessonService;
 import com.example.educationCrm.service.SchoolService;
+import com.example.educationCrm.service.StudentService;
 import com.example.educationCrm.service.TeacherService;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,6 +28,10 @@ import java.util.List;
 public class TeacherController {
     private Teacher teacher;
     private Teacher selectedTeacher;
+    private Lesson selectedLesson;
+
+    private List<Student> students;
+    private List<Student> selectedStudents;
     private List<Teacher> teacherList;
     private List<Lesson> lessonList;
     private List<School> schoolList;
@@ -35,6 +42,8 @@ public class TeacherController {
     private LessonService lessonService;
     @Autowired
     private SchoolService schoolService;
+    @Autowired
+    private StudentService studentService;
 
     @PostConstruct
     public void init(){
@@ -42,6 +51,7 @@ public class TeacherController {
         this.teacherList = teacherService.findAll();
         this.lessonList = lessonService.findAll();
         this.schoolList = schoolService.findAll();
+        this.students = studentService.findAllStudent();
     }
 
     public void save (){
@@ -68,6 +78,19 @@ public class TeacherController {
 
     public void selectTeacher(Teacher teacher){
         this.selectedTeacher =teacher;
+    }
+
+    public void changeLesson(){
+        this.teacherList = this.teacherService.findTeacherByLesson(this.selectedLesson);
+    }
+
+    public void assignStudent(){
+        this.selectedTeacher.setStudents(this.selectedStudents);
+        this.teacherService.save(this.selectedTeacher);
+        infoMessage("Atama Tamamlandı.", "Atama yapılan öğretmen : ",this.selectedTeacher);
+        this.selectedTeacher = new Teacher();
+        this.selectedLesson = new Lesson();
+        this.selectedStudents = new ArrayList<>();
     }
 
     public void infoMessage(String summary, String detail, Teacher teacher) {
